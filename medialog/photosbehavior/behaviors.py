a5b3c244726778144010a828da79e8ee19b1cb97
Espen Moe-Nilssen <espen@medialog.no>
2015-07-24 15:14:22 +0200

    GRR

--- medialog/photosbehavior/behaviors.py
+++ medialog/photosbehavior/behaviors.py
@@ -4,37 +4,42 @@ from zope.interface import alsoProvides
 from z3c.form import interfaces
 from plone.directives import form
 from plone.namedfile import field as namedfile
-from plone.app.contenttypes.behaviors import leadimage.py
 
+#do I need this ?
+from plone.supermodel import model
+from plone.dexterity.interfaces import IDexterityContent
 
 from collective.z3cform.datagridfield import DataGridFieldFactory 
 from collective.z3cform.datagridfield import DictRow
 from plone.autoform.interfaces import IFormFieldProvider
 
-from z3c.form.browser.multi import MultiWidget
+#from z3c.form.browser.multi import MultiWidget
 
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('medialog.photosbehavior')
 
+class IPhoto(model.Schema):
 
-class IImagePair(form.Schema):
-      
     image = namedfile.NamedBlobImage(
-    title = _(u'image', 'Image'),
+        title=_(u"Photo"),
+        description=u"",
         required=False,
     )
-    
-    title = schema.TextLine(
-        title=_(u'image_title', 'Image Title'),
-        required=False
+
+    image_title = schema.TextLine(
+        title=_(u"Photo Title"),
+        description=u"",
+        required=False,
     )
-    
-    description = schema.TextLine(
-        title=_(u'image_description', 'Image Description'),
-        required=False
+
+    image_description = schema.TextLine(
+        title=_(u"Photo Description"),
+        description=u"",
+        required=False,
     )
-    
- 
+
+alsoProvides(IPhoto, IFormFieldProvider)
+
  
 class IPhotosBehavior(form.Schema):
     """Adds settings to medialog.controlpanel
@@ -51,10 +56,8 @@ class IPhotosBehavior(form.Schema):
     image_pairs = schema.List(
         title = _(u"image_pairs", 
             default=u"Photos"),
-        value_type=DictRow(schema=IImagePair),
+        value_type=DictRow(schema=IPhoto),
     )
     
-    
-
 
 alsoProvides(IPhotosBehavior, IFormFieldProvider)
